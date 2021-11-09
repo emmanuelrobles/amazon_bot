17e9300832d6b1191f5a314a4374f123002e9a0b
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-11-09 10:14:27 -0500

    Added models

---
+++ models/Browsers.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class BrowsersEnum(Enum):
+    FIREFOX = 1
+    CHROME = 2
+    CHROMIUM = 3
---
+++ models/ProductOptions.py
@@ -0,0 +1,7 @@
+class ProductOptions:
+    def __init__(self, url: str, username: str, password: str, qty: int, price: float):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.qty = qty
+        self.price = price
