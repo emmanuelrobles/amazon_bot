40c112e344bcccacdc61a451210de278f86cbf46
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-11-09 10:16:04 -0500

    removed old class

--- services/ProductSraper.py
+++ services/ProductSraper.py
@@ -1,11 +1,8 @@
 import rx
-from rx import Observable
-from rx.subject import Subject
 
 from browser.Driver import get_a_driver
 from models.Browsers import BrowsersEnum
 from models.ProductOptions import ProductOptions
-import asyncio
 import rx.operators as ops
 
 from stores import Amazon
@@ -29,25 +26,3 @@ def get_price(options: ProductOptions, driver):
     except Exception as e:
         print(e)
 
-
-class Product:
-    def __init__(self, options: ProductOptions, browser: BrowsersEnum):
-        self.options = options
-        self.browserEnum = browser
-        self.subject = Subject()
-        driver = get_a_driver(self.browserEnum)
-        driver.get(self.options.url)
-        asyncio.ensure_future(self.__start_watching(driver))
-
-    async def __start_watching(self, driver):
-        while True:
-            await asyncio.sleep(1)
-            try:
-                driver.refresh()
-                if Amazon.check_price(driver) <= self.options.price:
-                    self.subject.on_next(driver)
-            except Exception as e:
-                print(e)
-
-    def on_product_observable(self) -> Observable:
-        return self.subject
