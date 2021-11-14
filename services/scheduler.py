8f84da2eadb1149d92754d0885e0e930c1c29c6d
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-11-14 11:19:30 -0500

    reworked scheduler

--- services/scheduler.py
+++ services/scheduler.py
@@ -1,25 +1,12 @@
 from typing import Optional, Callable
 
 import rx
-from selenium.webdriver.remote.webdriver import WebDriver
 
 from models.communication import Action
 import rx.operators as ops
 
 
-def init_scheduler(get_driver: Callable[[], WebDriver], try_get_metadata: Callable[[WebDriver], Optional[Action]], interval=1) -> rx.Observable:
-    driver = get_driver()
-
-    def get_metadata():
-        nonlocal driver
-        try:
-            return try_get_metadata(driver)
-        except Exception as e:
-            print(e)
-            driver = get_driver()
-            try_get_metadata(driver)
-
+def init_scheduler(get_metadata: Callable[[], Optional[Action]], interval=1) -> rx.Observable:
     return rx.interval(interval).pipe(
         ops.map(lambda i: get_metadata()),
-        ops.filter(lambda v: v is not None),
     )
