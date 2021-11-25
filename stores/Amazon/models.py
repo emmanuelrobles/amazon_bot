5d8d0bc9a6d204cedb0ebc4261b104407a867bbc
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-11-24 20:53:25 -0500

    added some models for amazon

--- stores/Amazon/models.py
+++ stores/Amazon/models.py
@@ -1,21 +1,27 @@
 from typing import Callable
 
-from models.product_options import ProductOptions
+
+class AmazonProduct:
+    def __init__(self, product_id: str, qty: int, price: float):
+        self.product_id = product_id
+        self.url = f'https://www.amazon.com/gp/aod/ajax/ref=auto_load_aod?asin={product_id}&pc=dp'
+        self.qty = qty
+        self.price = price
 
 
 class AmazonSiteData:
-    def __init__(self, price_found: float, add_to_cart_callback: Callable[[], str]):
+    def __init__(self, price_found: float, offer_id_callback: Callable[[], str]):
         self.price_found = price_found
-        self.add_to_cart_callback = add_to_cart_callback
+        self.offer_id_callback = offer_id_callback
 
 
 class AmazonProductFoundAction:
-    def __init__(self, site_data: AmazonSiteData, options: ProductOptions):
+    def __init__(self, site_data: AmazonSiteData, product: AmazonProduct):
         self.site_data = site_data
-        self.options = options
+        self.product = product
 
 
 class AmazonProductNotFoundAction:
-    def __init__(self, error_msg: str, options: ProductOptions):
+    def __init__(self, error_msg: str, product: AmazonProduct):
         self.error_msg = error_msg
-        self.options = options
+        self.product = product
